6b77d78a5e0975c16eea86d2cdf24f920c95cc1b
qsaval <115545576+qsaval@users.noreply.github.com>
2023-11-13 15:20:02 +0100

    modif

--- src/component/CommandeUser.jsx
+++ src/component/CommandeUser.jsx
@@ -1,4 +1,3 @@
-import {ListeProduit} from './ListeProduit'
 import {useEffect, useState} from "react";
 
 
@@ -24,7 +23,6 @@ export function CommandeUser({id}){
                 <thead>
                 <tr>
                     <th scope="col">Date de la commande</th>
-                    <th scope="col">Commande</th>
                     <th scope="col">Etat</th>
                     <th scope="col">Adresse</th>
                     <th scope="col">Ville</th>
@@ -34,7 +32,6 @@ export function CommandeUser({id}){
                 <tbody>
                 {data.map(c => (<tr key={c.id}>
                     <td>{c.date_commande}</td>
-                    <td><ListeProduit value={c.id}/></td>
                     <td>{c.etat_commande}</td>
                     <td>{c.adresse_facture}</td>
                     <td>{c.ville_facture}</td>
